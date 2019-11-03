a14a74eeb6d8002ad5cf8f5f3dfa6089172ac7b3
cluelesswalnut <ernst.sukram@gmail.com>
2019-11-03 18:21:32 -0500

    identified league teams section of HTML

--- DaytonLeagueStats.py
+++ DaytonLeagueStats.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://cincyultimate.org/leagues/2019-fall-dayton/teams")
+page = requests.get("https://cincyultimate.org/leagues/2019-summer-dayton/teams")
 print(page.status_code)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 # print(soup.prettify())
+print(list(soup.find("div", {"class": "league-teams"}).children))
 
 respons = requests.get('https://cincyultimate.org/leagues/team_players', {'team_id': '1670'})
-print(respons.content)
+# print(respons.content)
