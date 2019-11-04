fc57541ec48a8a1aa74c8747ee1f21bced5db79f
cluelesswalnut <ernst.sukram@gmail.com>
2019-11-03 20:40:04 -0500

    Add file to sum player/captian data

--- DaytonLeagueStats.py
+++ DaytonLeagueStats.py
@@ -1,3 +1,12 @@
+# create json with format:
+# league name
+#  - team name
+#   - wins
+#   - loses
+#   - ties
+#   - captians
+#   - players
+
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -8,38 +17,43 @@ soup = BeautifulSoup(page.content, 'html.parser')
 leagues = soup.find_all("table", {"class":"table table-hover"})[1].find_all('a')
 
 leagues = [i['href'] for i in leagues]
-leagues = [i for i in leagues if "dayton" in i.lower()]
+leagues = [i+'/teams' for i in leagues if "dayton" in i.lower()]
+del leagues[0]
 print(leagues)
 
 data = {}
 
-for league in leagues:
-
-    data[league] = {}
-    page = requests.get(league)
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    for index, teamHtml in enumerate(list(soup.find("div", {"class": "league-teams"}).children)):
-        if(index % 2 != 1):
-            continue
-        name = teamHtml.find("p", {"class":"title"}).contents[0]
-        # print(name)
-        data[leagues][name] = {}
-        record = teamHtml.find_all("strong", {"class":"text-info"})[1].contents[0].split(" - ")
-        # print(record)
-        data[leagues][name]['wins'] = record[0]
-        data[leagues][name]['loses'] = record[1]
-        data[leagues][name]['ties'] = record[2]
-        captians = [i.contents[1] for i in teamHtml.find_all('a', {"target":"_new"})]
-        # print(captians)
-        data[leagues][name]['captians'] = captians
-        teamId = teamHtml.find('a', {"data-team":True})['data-team']
-        playerResponse = requests.get('https://cincyultimate.org/leagues/team_players', {'team_id': teamId})
-        playerResponse = str(playerResponse.content).replace(r"\\", "")
-        playerSoup = BeautifulSoup(playerResponse, 'html.parser')
-        playersList = [i.contents[1].contents[0] for i in playerSoup.find_all('a')]
-        # print(playersList)
-        data[leagues][name]['players'] = playersList
+for index, league in enumerate(leagues):
+    try:
+        print(league)
+        print(1.0 * index / len(leagues))
+        data[league] = {}
+        page = requests.get(league)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        for index, teamHtml in enumerate(list(soup.find("div", {"class": "league-teams"}).children)):
+            if(index % 2 != 1):
+                continue
+            name = teamHtml.find("p", {"class":"title"}).contents[0]
+            # print(name)
+            data[league][name] = {}
+            record = teamHtml.find_all("strong", {"class":"text-info"})[1].contents[0].split(" - ")
+            # print(record)
+            data[league][name]['wins'] = record[0]
+            data[league][name]['loses'] = record[1]
+            data[league][name]['ties'] = record[2]
+            captians = [i.contents[1] for i in teamHtml.find_all('a', {"target":"_new"})]
+            # print(captians)
+            data[league][name]['captians'] = captians
+            teamId = teamHtml.find('a', {"data-team":True})['data-team']
+            playerResponse = requests.get('https://cincyultimate.org/leagues/team_players', {'team_id': teamId})
+            playerResponse = str(playerResponse.content).replace(r"\\", "")
+            playerSoup = BeautifulSoup(playerResponse, 'html.parser')
+            playersList = [i.contents[1].contents[0] for i in playerSoup.find_all('a')]
+            # print(playersList)
+            data[league][name]['players'] = playersList
+    except Exception as e:
+        print("_____________________FAILED_______________________ on " + league)
+        print(str(e))
 
 with open('DaytonLeaugeData.txt', 'w') as outFile:
     json.dump(data, outFile)
---
+++ ProcessData.py
@@ -0,0 +1,51 @@
+# create json with format:
+# league name
+#  - team name
+#   - wins
+#   - loses
+#   - ties
+#   - captians
+#   - players
+
+import json
+
+def sumStats(player, playerStatsDict, teamDict):
+    if player not in playerStatsDict:
+        playerStatsDict[player] = {}
+        playerStatsDict[player]['wins'] = 0
+        playerStatsDict[player]['loses'] = 0
+        playerStatsDict[player]['loses'] = 0
+        playerStatsDict[player]['nLeagues'] = 0
+        playerStatsDict[player]['eachLeagueWins'] = []
+        playerStatsDict[player]['eachLeagueLoses'] = []
+        playerStatsDict[player]['eachLeagueTies'] = []
+    playerStatsDict[player]['wins'] += int(teamDict['wins'])
+    playerStatsDict[player]['loses'] += int(teamDict['loses'])
+    playerStatsDict[player]['loses'] += int(teamDict['ties'])
+    playerStatsDict[player]['nLeagues'] += 1
+    playerStatsDict[player]['eachLeagueWins'].append(int(teamDict['wins']))
+    playerStatsDict[player]['eachLeagueLoses'].append(int(teamDict['loses']))
+    playerStatsDict[player]['eachLeagueTies'].append(int(teamDict['ties']))
+
+with open('DaytonLeaugeDataFirst.txt', 'r') as inFile:
+    data = json.load(inFile)
+
+print(data.keys())
+
+playerStats = {}
+captianStats = {}
+
+for leagueKey, leagueValue in data.items():
+    for teamKey, teamValue in leagueValue.items():
+        captians = teamValue['captians']
+        playerList = teamValue['players']
+        for player in playerList:
+            sumStats(player, playerStats, teamValue)
+        for captian in captians:
+            sumStats(captian, captianStats, teamValue)
+
+with open('PlayerStats.txt', 'w') as outFile:
+    json.dump(playerStats, outFile)
+
+with open('CaptianStats.txt', 'w') as outFile:
+    json.dump(captianStats, outFile)
