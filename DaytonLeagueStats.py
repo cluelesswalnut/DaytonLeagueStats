3294a4192a4ab630d7582347ca09a8b8a394d070
cluelesswalnut <ernst.sukram@gmail.com>
2019-11-03 19:51:34 -0500

    Setup to read data for all dayton leagues

--- DaytonLeagueStats.py
+++ DaytonLeagueStats.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 page = requests.get("https://cincyultimate.org/profile/thomas-brewster")
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -10,30 +11,59 @@ leagues = [i['href'] for i in leagues]
 leagues = [i for i in leagues if "dayton" in i.lower()]
 print(leagues)
 
+data = {}
 
-page = requests.get("https://cincyultimate.org/leagues/2019-summer-dayton/teams")
-print(page.status_code)
+for league in leagues:
 
-soup = BeautifulSoup(page.content, 'html.parser')
-# print(soup.prettify())
-# print(list(soup.find("div", {"class": "league-teams"}).children)[1].find("p", {"class":"title"}))
+    data[league] = {}
+    page = requests.get(league)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-for index, teamHtml in enumerate(list(soup.find("div", {"class": "league-teams"}).children)):
-    if(index % 2 != 1):
-        continue
-    name = teamHtml.find("p", {"class":"title"}).contents
-    print(name)
-    record = teamHtml.find_all("strong", {"class":"text-info"})[1].contents[0].split(" - ")
-    print(record)
-    captians = [i.contents[1] for i in teamHtml.find_all('a', {"target":"_new"})]
-    print(captians)
-    teamId = teamHtml.find('a', {"data-team":True})['data-team']
-    playerResponse = requests.get('https://cincyultimate.org/leagues/team_players', {'team_id': teamId})
-    playerResponse = str(playerResponse.content).replace(r"\\", "")
-    playerSoup = BeautifulSoup(playerResponse, 'html.parser')
-    playersList = [i.contents[1].contents for i in playerSoup.find_all('a')]
-    print(playersList)
+    for index, teamHtml in enumerate(list(soup.find("div", {"class": "league-teams"}).children)):
+        if(index % 2 != 1):
+            continue
+        name = teamHtml.find("p", {"class":"title"}).contents[0]
+        # print(name)
+        data[leagues][name] = {}
+        record = teamHtml.find_all("strong", {"class":"text-info"})[1].contents[0].split(" - ")
+        # print(record)
+        data[leagues][name]['wins'] = record[0]
+        data[leagues][name]['loses'] = record[1]
+        data[leagues][name]['ties'] = record[2]
+        captians = [i.contents[1] for i in teamHtml.find_all('a', {"target":"_new"})]
+        # print(captians)
+        data[leagues][name]['captians'] = captians
+        teamId = teamHtml.find('a', {"data-team":True})['data-team']
+        playerResponse = requests.get('https://cincyultimate.org/leagues/team_players', {'team_id': teamId})
+        playerResponse = str(playerResponse.content).replace(r"\\", "")
+        playerSoup = BeautifulSoup(playerResponse, 'html.parser')
+        playersList = [i.contents[1].contents[0] for i in playerSoup.find_all('a')]
+        # print(playersList)
+        data[leagues][name]['players'] = playersList
 
+with open('DaytonLeaugeData.txt', 'w') as outFile:
+    json.dump(data, outFile)
 
-response = requests.get('https://cincyultimate.org/leagues/team_players', {'team_id': '1670'})
-# print(response.content.prettify())
+test = False
+if test:
+    import requests
+    from bs4 import BeautifulSoup
+    page = requests.get("https://cincyultimate.org/leagues/2019-summer-dayton/teams")
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    for index, teamHtml in enumerate(list(soup.find("div", {"class": "league-teams"}).children)):
+        if(index % 2 != 1):
+            continue
+        name = teamHtml.find("p", {"class":"title"}).contents[0]
+        print(name)
+        # data[leagues]['teams'].append(name)
+        record = teamHtml.find_all("strong", {"class":"text-info"})[1].contents[0].split(" - ")
+        print(record)
+        captians = [i.contents[1] for i in teamHtml.find_all('a', {"target":"_new"})]
+        print(captians)
+        teamId = teamHtml.find('a', {"data-team":True})['data-team']
+        playerResponse = requests.get('https://cincyultimate.org/leagues/team_players', {'team_id': teamId})
+        playerResponse = str(playerResponse.content).replace(r"\\", "")
+        playerSoup = BeautifulSoup(playerResponse, 'html.parser')
+        playersList = [i.contents[1].contents[0] for i in playerSoup.find_all('a')]
+        print(playersList)
