fea1ae5e75f57c86cd32b4fa3a7069494c5df897
Marky <marky@mark.com>
2022-06-12 02:38:41 -0400

    Add comments and update naming in OutPutTables.py

--- OutPutTables.py
+++ OutPutTables.py
@@ -1,6 +1,15 @@
+# This script takes output from `ProcessData.py` and formats it in tables
+# that are easy to process with excel or similar tools
+
 import json
 
-def createTable(statsFile, outputPath):
+def create_table(statsFile, outputPath):
+    """
+    Create a table out of player data in JSON files
+
+    @param  statsFile   The JSON file containing the stats to tabulate
+    @param  outputPath  The output file to write the table to
+    """
     with open(statsFile, 'r') as inFile:
         playerData = json.load(inFile)
 
@@ -20,34 +29,6 @@ def createTable(statsFile, outputPath):
             outFile.write(str(stats['wins']/(stats['loses'] + stats['ties'] + stats['wins'])))
             outFile.write('\n')
 
-# createTable('PlayerStats.txt', 'PlayerStatsTable.txt')
-# createTable('CaptianStats.txt', 'CaptianStatsTable.txt')
-
-
-with open('DaytonLeaugeDataFirst.txt', 'r') as inFile:
-    data = json.load(inFile)
-
-with open('DataTable.txt', 'w') as outFile:
-    outFile.write('PlayerName' + '\t' + 'League' + '\t' + 'teamName' + '\t' + 'captain1' + '\t' + 'captain2' + '\t' + 'wins' + '\t' + 'loses' + '\t' + 'ties' + '\n')
-    for leagueKey, leagueValue in data.items():
-        for teamKey, teamValue in leagueValue.items():
-            captians = teamValue['captians']
-            playerList = teamValue['players']
-            for player in playerList:
-                print(str(player))
-                outFile.write(str(player))
-                outFile.write('\t')
-                outFile.write(str(leagueKey))
-                outFile.write('\t')
-                outFile.write(str(teamKey))
-                outFile.write('\t')
-                outFile.write(captians[0])
-                outFile.write('\t')
-                outFile.write(captians[1] if len(captians) > 1 else 'None')
-                outFile.write('\t')
-                outFile.write(str(teamValue['wins']))
-                outFile.write('\t')
-                outFile.write(str(teamValue['loses']))
-                outFile.write('\t')
-                outFile.write(str(teamValue['ties']))
-                outFile.write('\n')
+# create the tables from the data processed in `ProcessData.py`
+create_table('PlayerStats.json', 'PlayerStatsTable.txt')
+create_table('CaptainStats.json', 'CaptainStatsTable.txt')
