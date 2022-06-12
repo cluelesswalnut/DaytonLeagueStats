0aaf4664aade3aee153ad23a1ec550f1a9d52817
Marky <marky@mark.com>
2022-06-12 02:22:11 -0400

    Add comments and fix naming in DaytonLeagueStats.py

--- DaytonLeagueStats.py
+++ DaytonLeagueStats.py
@@ -1,83 +1,83 @@
-# create json with format:
-# league name
-#  - team name
-#   - wins
-#   - loses
-#   - ties
-#   - captians
-#   - players
+# This script scrapes the cincyultimate website and creates json with format:
+# { 
+#     "league name":
+#     {
+#         "team name":
+#         {
+#             wins: int,
+#             loses: int,
+#             ties: int,
+#             captains: [],
+#             players: []
+#         },
+#         more teams...
+#     },
+#     more leagues...
+# }
 
-import requests
 from bs4 import BeautifulSoup
 import json
+import requests
 
-page = requests.get("https://cincyultimate.org/profile/thomas-brewster")
+# Parse all the leagues this player has been apart of.
+ROOT_PLAYER_PROFILE = "https://cincyultimate.org/profile/thomas-brewster-19"
+
+page = requests.get(ROOT_PLAYER_PROFILE)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+# The league urls are in the second "table table-hover" on the page
 leagues = soup.find_all("table", {"class":"table table-hover"})[1].find_all('a')
-
+# get the urls for each league
 leagues = [i['href'] for i in leagues]
+# only keep dayton leagues and append '/teams' to access the teams that were part of the league
 leagues = [i+'/teams' for i in leagues if "dayton" in i.lower()]
-del leagues[0]
-print(leagues)
 
 data = {}
 
-for index, league in enumerate(leagues):
+for j, league in enumerate(leagues):
+    # because we are scraping info from a website the format may be unpredictable and we may run into errors
+    # catch the error and move on to the next league
     try:
+        # print progress
         print(league)
-        print(1.0 * index / len(leagues))
+        print(1.0 * j / len(leagues))
+
         data[league] = {}
         page = requests.get(league)
         soup = BeautifulSoup(page.content, 'html.parser')
         for index, teamHtml in enumerate(list(soup.find("div", {"class": "league-teams"}).children)):
+            # every other html child is a new line, skip it
             if(index % 2 != 1):
                 continue
+
             name = teamHtml.find("p", {"class":"title"}).contents[0]
-            # print(name)
             data[league][name] = {}
+
+            # the second "text-info" gives the record in the form or 'W - L - T'
             record = teamHtml.find_all("strong", {"class":"text-info"})[1].contents[0].split(" - ")
-            # print(record)
             data[league][name]['wins'] = record[0]
             data[league][name]['loses'] = record[1]
             data[league][name]['ties'] = record[2]
-            captians = [i.contents[1] for i in teamHtml.find_all('a', {"target":"_new"})]
-            # print(captians)
-            data[league][name]['captians'] = captians
+
+            # the captains name is the second field in the 'target = "_new"' field
+            captains = [i.contents[1] for i in teamHtml.find_all('a', {"target":"_new"})]
+            data[league][name]['captains'] = captains
+
+            # the team players is a pop up window. The information needs to be retrieved via a
+            # separate request to: "https://cincyultimate.org/leagues/team_players?team_id=<teamId>"
             teamId = teamHtml.find('a', {"data-team":True})['data-team']
             playerResponse = requests.get('https://cincyultimate.org/leagues/team_players', {'team_id': teamId})
             playerResponse = str(playerResponse.content).replace(r"\\", "")
             playerSoup = BeautifulSoup(playerResponse, 'html.parser')
+
+            # the name is embedded in a few layers of content
             playersList = [i.contents[1].contents[0] for i in playerSoup.find_all('a')]
-            # print(playersList)
             data[league][name]['players'] = playersList
+
     except Exception as e:
         print("_____________________FAILED_______________________ on " + league)
         print(str(e))
 
-with open('DaytonLeaugeData.txt', 'w') as outFile:
+# output the scraped data to a json file
+with open('DaytonLeagueData.json', 'w') as outFile:
     json.dump(data, outFile)
-
-test = False
-if test:
-    import requests
-    from bs4 import BeautifulSoup
-    page = requests.get("https://cincyultimate.org/leagues/2019-summer-dayton/teams")
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    for index, teamHtml in enumerate(list(soup.find("div", {"class": "league-teams"}).children)):
-        if(index % 2 != 1):
-            continue
-        name = teamHtml.find("p", {"class":"title"}).contents[0]
-        print(name)
-        # data[leagues]['teams'].append(name)
-        record = teamHtml.find_all("strong", {"class":"text-info"})[1].contents[0].split(" - ")
-        print(record)
-        captians = [i.contents[1] for i in teamHtml.find_all('a', {"target":"_new"})]
-        print(captians)
-        teamId = teamHtml.find('a', {"data-team":True})['data-team']
-        playerResponse = requests.get('https://cincyultimate.org/leagues/team_players', {'team_id': teamId})
-        playerResponse = str(playerResponse.content).replace(r"\\", "")
-        playerSoup = BeautifulSoup(playerResponse, 'html.parser')
-        playersList = [i.contents[1].contents[0] for i in playerSoup.find_all('a')]
-        print(playersList)
